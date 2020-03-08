65b0d0ed33e02385ba9d35aca848a30188ec0372
Alice-ucm <ajoy@ucm.es>
2020-03-08 23:39:38 +0100

    Interface-inicio

--- take_photo.php
+++ take_photo.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Demo Take a photo for QuickList</title>
+    <title>Proyecto MIT</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
