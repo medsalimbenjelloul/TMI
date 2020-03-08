dae004aaa5b08db5f58ba2df0d1f045b2883121d
medsalimbenjelloul <48842656+medsalimbenjelloul@users.noreply.github.com>
2020-03-08 18:07:37 +0100

    Add files via upload
    Un script php para detectar las caras en una imagen y analizar sus emociones. Hay que tener Http_Request 2 y he usado xammp para hacer la preuba. Se puede usar no importa un php web based program

---
+++ test.php
@@ -0,0 +1,57 @@
+<html>
+    <head>
+        <title>Face Detect Sample</title>
+    </head>
+    <body>
+<?php
+//Subscription key
+$ocpApimSubscriptionKey = 'c94f664002cd449fbd36cf8fe08c784e';
+
+//endpoint url
+$uriBase = 'https://westeurope.api.cognitive.microsoft.com/face/v1.0/';
+
+// un ejemplo de un imagen
+$imageUrl =
+    'https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg';
+
+require_once 'HTTP/Request2.php';
+//api de detectar (Face Detect)
+$request = new Http_Request2($uriBase . '/detect');
+$url = $request->getUrl();
+//Headers
+$headers = array(
+    // Request headers
+    'Content-Type' => 'application/json',
+    'Ocp-Apim-Subscription-Key' => $ocpApimSubscriptionKey
+);
+$request->setHeader($headers);
+//Parametros
+$parameters = array(
+    // Request parameters
+    'returnFaceId' => 'true',
+    'returnFaceLandmarks' => 'false',
+    'returnFaceAttributes' => 'age,gender,headPose,smile,facialHair,glasses,' .
+        'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise');
+$url->setQueryVariables($parameters);
+//Post Method
+$request->setMethod(HTTP_Request2::METHOD_POST);
+
+//Body
+$body = json_encode(array('url' => $imageUrl));
+
+
+$request->setBody($body);
+//Repuesta
+try
+{
+    $response = $request->send();
+    echo "<pre>" .
+        json_encode(json_decode($response->getBody()), JSON_PRETTY_PRINT) . "</pre>";
+}
+catch (HttpException $ex)
+{
+    echo "<pre>" . $ex . "</pre>";
+}
+?>
+</body>
+</html>
